9df79b0a710bd1b475f6c2796128f4960b7c3a8a
rcilomba <rcilomba@yahoo.com>
2026-08-13 23:26:21 +0200

    feat: implement proof of work mining

--- src/blockchain/Blockchain.js
+++ src/blockchain/Blockchain.js
@@ -11,4 +11,15 @@ export class Blockchain {
 
     return crypto.createHash('sha256').update(blockData).digest('hex');
   }
+
+  mineBlock(block, difficulty) {
+    const target = '0'.repeat(difficulty);
+
+    while (!block.hash.startsWith(target)) {
+      block.nonce += 1;
+      block.hash = this.calculateHash(block);
+    }
+
+    return block;
+  }
 }
